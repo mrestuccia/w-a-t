b503a731aac4be9471c57818c3291d5c261869b6
Summer Guan <summerguan@ip-192-168-5-22.ec2.internal>
2017-05-25 19:41:50 -0400

    add exchangeTokenForUser

--- src/Routes.js
+++ src/Routes.js
@@ -6,8 +6,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Login from './components/LoginPage'; 
-
-//import { exchangeTokenForUser } from './redux/reducers/userReducer';
+// import { exchangeTokenForUser } from './actions/loginActions.js';
 import { loadGroups } from './redux/reducers/groupReducer';
 
 
@@ -25,7 +24,7 @@ const Routes = ({ bootstrap })=> {
 
 const mapDispatchToProps = (dispatch)=> {
   const bootstrap = ()=> {
-    //dispatch(exchangeTokenForUser())
+    // dispatch(exchangeTokenForUser())
     //  .then( user => console.log(user));
     dispatch(loadGroups(1));
   };
