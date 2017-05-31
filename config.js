a697a48d8a4dfd040f0714cd89a609b7036a959b
Mauro Restuccia <mrestuccia@mac.com>
2017-05-31 15:10:40 -0400

    fix: Minor correction to use Heroku
    added process env first on the list to take it from heroku configuration is available

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "clientID": "895713410708-gcrk77g784ctmgv573m4q71dnt1q605k.apps.googleusercontent.com",
-"clientSecret": "NHPVCjB8uOef4VlFawbVTYAk",
-"callbackURL": "http://localhost:3000/auth/google/callback"
+    "clientSecret": "NHPVCjB8uOef4VlFawbVTYAk",
+    "callbackURL": "http://localhost:3000/auth/google/callback"
 }
 
 
